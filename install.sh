c61a0af263b41d3b6cd147bee6f31c236b3b39e3
bigwheel <k.bigwheel+eng@gmail.com>
2016-12-25 16:51:04 +0900

    tpmを追加

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# http://qiita.com/yudoufu/items/48cb6fb71e5b498b2532
+script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+
+mkdir -p $HOME/.tmux/plugins/
+ln -s tpm $HOME/.tmux/plugins/tpm
